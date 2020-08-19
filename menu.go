fc20a70e16050440b8e908f4e0169052e811b994
rexus76 <annas.adharuqudni@gmail.com>
2020-08-20 01:16:47 +0700

    menu

---
+++ menu.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var pilihan int
+	var kembali string
+	var jumlahData int
+	var nama [10]string
+	var nim [10]string
+	var ipk [10]float32
+	k := 0
+
+	iya := true
+	for iya {
+		iya = true
+		pilihan = 0
+		fmt.Println("::::::::::::: MENU :::::::::::::")
+		fmt.Println("1. INPUT DATA")
+		fmt.Println("2. TAMPILKAN DATA")
+		fmt.Println("3. KELUAR")
+		fmt.Print("PILIH : ")
+		fmt.Scan(&pilihan)
+
+		if pilihan == 1 {
+			fmt.Println("INPUT DATA")
+			fmt.Print("banyak data : ")
+			fmt.Scan(&jumlahData)
+			for i := k; i < jumlahData; i++ {
+				fmt.Print("nama : ")
+				fmt.Scan(&nama[i])
+				fmt.Print("nim : ")
+				fmt.Scan(&nim[i])
+				fmt.Print("ipk : ")
+				fmt.Scan(&ipk[i])
+				k++
+			}
+			fmt.Print("kembali? (y/n) : ")
+			fmt.Scan(&kembali)
+			if kembali == "y" {
+				iya == true
+			} else {
+				iya == false
+			}
+		}
+		if pilihan == 2 {
+			for i := 0; i < len(nama); i++ {
+				fmt.Println("nama : ", nama[i])
+				fmt.Println("nim : ", nim[i])
+				fmt.Println("ipk : ", ipk[i])
+			}
+		}
+		if pilihan == 3 {
+			os.Exit(0)
+		}
+	}
+}
